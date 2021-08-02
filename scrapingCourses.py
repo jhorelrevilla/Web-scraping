e959178496375d603423faefaf185ad722373fa3
jhorelrevilla <jhorel.revilla@ucsp.edu.pe>
2021-08-02 18:19:21 -0500

    Update scrapingCourses.py

--- scrapingCourses.py
+++ scrapingCourses.py
@@ -15,9 +15,7 @@ def getCoupons():
     container=soup.find("div",class_="w-screen sm:w-full md:full lg:w-full xl:w-full mx-auto grid grid-cols-1 px-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4")
     list_courses=container.findAll("a", class_="c-card block bg-white shadow-md hover:shadow-xl rounded-lg overflow-hidden")
     for i in list_courses:
-        #list_url.append(i.get('href'))
         name=i.find_all("h2") 
-        #list_names.append(name[0].get_text())
         list_course_url.append([name[0].get_text(),i.get('href')])
 
 getCoupons()
